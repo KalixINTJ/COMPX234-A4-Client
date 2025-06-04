da3c5554fd5c8a3020a5ebd530f6e7577ea0701e
KalixINTJ <140989261+KalixINTJ@users.noreply.github.com>
2025-06-04 22:30:31 +0800

    I updated the code, implemented the file list reading function, and added methods for sending and receiving messages.

--- UDPclient.java
+++ UDPclient.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class UDPclient
 {
@@ -10,6 +12,8 @@ public class UDPclient
     public String fileListName;
     //The UDP socket, used for communication.
     public DatagramSocket cSocket;
+    //Make sure there is enough space to receive these data packets.
+    public int bigBufferSize = 10000;
 
     public UDPclient(String h, int p, String f)
     {
@@ -29,9 +33,59 @@ public class UDPclient
         }
     }
 
+    public List<String> readFileList(String name) throws IOException
+    {
+        //To create a list of file names.
+        List<String> files = new ArrayList<>();
+        //To use BufferedReader to read the file list.
+        try(BufferedReader reader = new BufferedReader(new FileReader(name)))
+        {
+            String line;
+            while((line = reader.readLine()) != null)
+            {
+                //To remove the first and last whitespace characters and ignore the blank lines.
+                line = line.trim();
+                if(!line.isEmpty())
+                {
+                    files.add(line);
+                }
+            }
+        }
+        return files;
+    }
+    //To send a message and wait for receiving.
+    public String sendAndReceive(String m, String h, int p) throws IOException
+    {
+        //TO parse the address of the target host.
+        InetAddress sAdress = InetAddress.getByName(h);
+        //To convert the message to a byte array and create a receiving buffer.
+        byte[] sendData = m.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, sAdress, p);
+        byte[] rBuffer = new byte[bigBufferSize];
+        DatagramPacket receivPacket = new DatagramPacket(rBuffer, rBuffer.length);
+        //To send data packets and receive responses from the server.
+        cSocket.send(sendPacket);
+        System.out.println("Send: " + m);
+        cSocket.receive(receivPacket);
+        String response = new String(receivPacket.getData(), 0, receivPacket.getLength());
+        System.out.println("Receive: " + response);
+        return response;
+    }
     public void run()
     {
-       //To add the file list and download the files.
+        try
+        {
+            //To read the file list and obtain the list of file names that need to be downloaded.
+            List<String> files = readFileList(fileListName);
+            for(String file : files)
+            {
+                //To traverse the file list and download each file in sequence.
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Client error: " + e.getMessage());
+        }
     }
 
     public void main(String[] args) 
